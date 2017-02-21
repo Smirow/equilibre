c9dbad74817c205ecaab31fb2c3366b2ab421c86
Smirow <magued1509@gmail.com>
2017-02-21 12:01:08 +0100

    Some fixes in the main

--- src/main.c
+++ src/main.c
@@ -19,16 +19,19 @@ int main(void) {
 
 	char *p, s[100];
     int n;
+
 	printf("Entrez une couleur : ");
     while (!boolwin && fgets(s, sizeof(s), stdin)) {
         n = strtol(s, &p, 10);
         if (p == s || *p != '\n') {
             printf("Entrez un nombre.\n");
-        } else if(n >= 1 && n <= 7) {
+        }
+		else if(n >= 1 && n <= 7) {
 			changeCC(M, n, size);
 			boolwin = win(M, size);
 			printMatrix(M, size);
-		} else {
+		}
+		else {
 			printf("Entrez un nombre entre 1 et 6.\n");
 		}
 		printf("RENTRE UNE COULEUR STP : ");
