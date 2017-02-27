dbd9bfad4940c6c4e9e26eda3070be59984cb6ea
Smirow <magued1509@gmail.com>
2017-02-27 09:59:20 +0100

    Couleur testing

--- test/main.c
+++ test/main.c
@@ -1,4 +1,5 @@
 #include "headers/grilleTest.h"
+#include "headers/couleurTest.h"
 #include <CUnit/Basic.h>
 
 
@@ -17,6 +18,7 @@ int clean_suite_1(void) {
 
 int main(void) {
     CU_pSuite suite_grille = NULL;
+    CU_pSuite suite_couleur = NULL;
 
     /* initialize the CUnit test registry */
     if (CUE_SUCCESS != CU_initialize_registry())
@@ -24,7 +26,8 @@ int main(void) {
 
     /* add a suite to the registry */
     suite_grille = CU_add_suite("Module Grille", init_suite_1, clean_suite_1);
-    if (NULL == suite_grille) {
+    suite_couleur = CU_add_suite("Module Couleur", init_suite_1, clean_suite_1);
+    if (NULL == suite_grille || NULL == suite_couleur) {
         CU_cleanup_registry();
         return CU_get_error();
     }
@@ -37,6 +40,14 @@ int main(void) {
         CU_cleanup_registry();
         return CU_get_error();
    }
+   if ((NULL == CU_add_test(suite_couleur, "test of replace()", test_replace)) ||
+   (NULL == CU_add_test(suite_couleur, "test of modifcoord()", test_modifcoord)) ||
+   (NULL == CU_add_test(suite_couleur, "test of emptyFIFO()", test_emptyFIFO)) ||
+   (NULL == CU_add_test(suite_couleur, "test of defile()", test_defile)) ||
+   (NULL == CU_add_test(suite_couleur, "test of win()", test_win))){
+       CU_cleanup_registry();
+       return CU_get_error();
+  }
 
    /* Run all tests using the CUnit Basic interface */
    CU_basic_set_mode(CU_BRM_VERBOSE);
---
+++ test/src/couleurTest.c
@@ -0,0 +1,51 @@
+#include "../../headers/couleur.h"
+#include "../headers/couleurTest.h"
+#include <CUnit/Basic.h>
+
+
+void test_replace(void) {
+    Matrix matrix = initMatrix(3);
+    coordonnees coord = modifcoord(1, 1);
+    Matrix matrix2 = initMatrix(3);
+    coordonnees coord2 = modifcoord(4, 1);
+    replace(matrix, 3, coord, 42);
+    replace(matrix2, 3, coord2, 42);
+    CU_ASSERT(matrix[1][1] == 42);
+    CU_ASSERT(matrix[0][0] == 0);
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            CU_ASSERT(matrix2[i][j] == 0);
+        }
+    }
+}
+
+void test_modifcoord(void) {
+    coordonnees coord = modifcoord(1, 42);
+    CU_ASSERT((coord.x == 1) && (coord.y == 42));
+    coord = modifcoord(42, 1);
+    CU_ASSERT((coord.x == 42) && (coord.y == 1));
+}
+
+void test_emptyFIFO(void) {
+    FIFO *f = initFIFOVide();
+    CU_ASSERT(emptyFIFO(f));
+    coordonnees coord = modifcoord(0, 0);
+    constructeur(f, coord);
+    CU_ASSERT_FALSE(emptyFIFO(f));
+}
+
+void test_defile(void) {
+    FIFO *f = initFIFOVide();
+    coordonnees coord = modifcoord(24, 42);
+    constructeur(f, coord);
+    coord = defile(f);
+    CU_ASSERT((coord.x == 24) && (coord.y == 42));
+    CU_ASSERT(emptyFIFO(f));
+}
+
+void test_win(void) {
+    Matrix matrix = initMatrix(3);
+    CU_ASSERT(win(matrix, 3));
+    matrix[1][2] = 3;
+    CU_ASSERT_FALSE(win(matrix, 3));
+}
