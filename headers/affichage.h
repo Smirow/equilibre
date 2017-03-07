f09f5b3d1e5f0126fa4a3f2a7cfb1d49852f04ac
Smirow <magued1509@gmail.com>
2017-03-07 11:55:52 +0100

    fixes header

--- headers/affichage.h
+++ headers/affichage.h
@@ -9,8 +9,6 @@
 
 #ifndef __COULEUR_H
 #define __COULEUR_H
-#include "../headers/file.h"
-#include "../headers/grille.h"
 
 void drawRectangle(SDL_Surface *ecran, int px, int py, int size, int r, int g, int b);
 void fillScreen(SDL_Surface *ecran, int r, int g, int b);
