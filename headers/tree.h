1d94c95dda0f92254be795c2e58b73ea3618fcd6
Smirow <magued1509@gmail.com>
2017-03-25 15:50:40 +0100

    Tree structure initial commit

---
+++ headers/tree.h
@@ -0,0 +1,23 @@
+#define NBSON 5
+
+typedef struct t_tree {
+       int val;
+       int nbSon;
+       int matrixSize;
+       Matrix matrix;
+       struct t_tree *tabSon[NBSON];
+} NNode, *NTree;
+
+#define TREENULL NULL
+
+NTree newNTree(int val);
+int nTreeNull(NTree a);
+NTree addSon(NTree a, NTree son);
+void freeNode(NTree node);
+void forceDeleteNTree(NTree tree);
+void truncateTree(NTree tree, int level);
+void freeTree(NTree tree);
+void printNTreeRec(NTree a, int p);
+void printNTree(NTree a);
+void removeSon(NTree father, int indice);
+void copyMatrixIntoNode(NTree node, Matrix matrixToCopy, int size);
